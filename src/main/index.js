45172cc051581f61fb39eff924487aaca458c6d1
张恒 <syl1998@hotmail.com>
2020-01-10 11:29:24 +0800

    删除多余注释

--- src/main/index.js
+++ src/main/index.js
@@ -19,28 +19,3 @@ app.on('window-all-closed', () => {
 app.on('browser-window-created', () => {
   console.log('window-created')
 })
-// app.on('activate', () => {
-//   if (mainWindow === null) {
-//     createWindow()
-//   }
-// })
-
-/**
- * Auto Updater
- *
- * Uncomment the following code below and install `electron-updater` to
- * support auto updating. Code Signing with a valid certificate is required.
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
- */
-
-/*
-import { autoUpdater } from 'electron-updater'
-
-autoUpdater.on('update-downloaded', () => {
-  autoUpdater.quitAndInstall()
-})
-
-app.on('ready', () => {
-  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
-})
- */
