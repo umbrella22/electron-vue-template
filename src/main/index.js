b2fa488cb26a09065c427e6d718ecd627cf415a7
张恒 <syl1998@hotmail.com>
2020-05-15 11:03:12 +0800

    添加浏览器拉起能力

--- src/main/index.js
+++ src/main/index.js
@@ -18,3 +18,12 @@ app.on('window-all-closed', () => {
 app.on('browser-window-created', () => {
   console.log('window-created')
 })
+
+if (process.defaultApp) {
+  if (process.argv.length >= 2) {
+    app.removeAsDefaultProtocolClient('electron-vue-template')
+    console.log('有于框架特殊性开发环境下无法使用')
+  }
+} else {
+  app.setAsDefaultProtocolClient('electron-vue-template')
+}
