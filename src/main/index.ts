52072456ebd904f61a65f95ef41ab1161d742b37
umbrella22 <1035765115@qq.com>
2020-08-20 12:42:02 +0800

    修改出现的跨域问题

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,6 +10,8 @@ function onAppReady () {
 }
 
 app.isReady() ? onAppReady() : app.on('ready', onAppReady)
+// 由于9.x版本问题，需要加入该配置关闭跨域问题
+app.commandLine.appendSwitch('disable-features', 'OutOfBlinkCors');
 
 app.on('window-all-closed', () => {
   // 所有平台均为所有窗口关闭就退出软件
