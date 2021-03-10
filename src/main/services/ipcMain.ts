38a5eb83a6a58b2f8d7f3b038326c978c6dd8cdd
umbrella22 <1035765115@qq.com>
2021-03-10 15:22:20 +0800

    同步master修改

--- src/main/services/ipcMain.ts
+++ src/main/services/ipcMain.ts
@@ -3,24 +3,24 @@ import Server from '../server'
 import { winURL } from '../config/StaticPath'
 
 export default {
-  Mainfunc (mainWindow: BrowserWindow, IsUseSysTitle: Boolean) {
-    ipcMain.handle('IsUseSysTitle', async () => {
+  Mainfunc (mainWindow: BrowserWindow, IsUseSysTitle: boolean) {
+    ipcMain.handle('IsUseSysTitle', async (event, args) => {
       return IsUseSysTitle
     })
-    ipcMain.handle('windows-mini', () => {
-      mainWindow.minimize()
+    ipcMain.handle('windows-mini', (event, args) => {
+      BrowserWindow.fromWebContents(event.sender)?.minimize()
     })
-    ipcMain.handle('window-max', async () => {
-      if (mainWindow.isMaximized()) {
-        mainWindow.restore()
+    ipcMain.handle('window-max', async (event, args) => {
+      if (BrowserWindow.fromWebContents(event.sender)?.isMaximized()) {
+        BrowserWindow.fromWebContents(event.sender)?.restore()
         return { status: false }
       } else {
-        mainWindow.maximize()
+        BrowserWindow.fromWebContents(event.sender)?.maximize()
         return { status: true }
       }
     })
-    ipcMain.handle('window-close', () => {
-      mainWindow.close()
+    ipcMain.handle('window-close', (event, args) => {
+      BrowserWindow.fromWebContents(event.sender)?.close()
     })
     ipcMain.handle('open-messagebox', async (event, arg) => {
       const res = await dialog.showMessageBox(mainWindow, {
@@ -69,6 +69,7 @@ export default {
         autoHideMenuBar: true,
         minWidth: 842,
         show: false,
+        frame: IsUseSysTitle,
         webPreferences: {
           nodeIntegration: true,
           contextIsolation: false,
