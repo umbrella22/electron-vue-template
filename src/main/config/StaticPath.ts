d54d198ac924133fa030984871c7fe50f14f8ee0
umbrella22 <1035765115@qq.com>
2021-03-12 18:28:56 +0800

    同步修改

--- src/main/config/StaticPath.ts
+++ src/main/config/StaticPath.ts
@@ -3,14 +3,11 @@ import { join } from 'path'
 import { DllFolder } from '@config/index'
 
 var __static: string
-var __lib: string
 if (process.env.NODE_ENV !== 'development') {
   __static = join(__dirname, '/static').replace(/\\/g, '\\\\')
-  __lib = join(__dirname, '..', '..', '..', '..', `${DllFolder}`).replace(/\\/g, '\\\\')
-} else {
-  __lib = process.env.libPath
+  process.env.libPath = join(__dirname, '..', '..', '..', '..', `${DllFolder}`).replace(/\\/g, '\\\\')
 }
 
 export const winURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}` : `file://${__dirname}/index.html`
 export const loadingURL = process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT}/static/loader.html` : `file://${__static}/loader.html`
-export const lib = __lib
+export const lib = process.env.libPath
