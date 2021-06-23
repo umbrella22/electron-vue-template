d1b657a0977d65b164172971119375b5c3ba494b
orzi! <1063614727@qq.com>
2021-06-23 19:43:10 +0800

    新增 i18n

--- src/renderer/i18n/languages/zh_CN.js
+++ src/renderer/i18n/languages/zh_CN.js
@@ -1,4 +1,28 @@
 export const lang = {
-    home: "语言",
-    name: "中文简体"
+    welcome: "欢迎进入本框架",
+    buttonTips: "您可以点击的按钮测试功能",
+    waitDataLoading: "等待数据读取",
+    about: {
+        system: "关于系统",
+        language: "语言：",
+        languageValue: "中文简体",
+        currentPagePath: "当前页面路径：",
+        currentPageName: "当前页面名称：",
+        vueVersion: "Vue版本：",
+        electronVersion: "Electron版本：",
+        nodeVersion: "Node版本：",
+        systemPlatform: "系统平台：",
+        systemVersion: "系统版本：",
+        systemArch: "系统位数：" 
+    },
+    buttons: {
+        console: "控制台打印",
+        checkUpdate: "检查更新",
+        checkUpdate2: "检查更新（第二种方法）",
+        startServer: "启动内置服务端",
+        stopServer: "关闭内置服务端",
+        viewMessage: "查看消息",
+        openNewWindow: "打开新窗口",
+        changeLanguage: "切换语言"
+    }
 }
