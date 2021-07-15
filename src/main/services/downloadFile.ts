96ad273bdfd95faa8ce6f3f50164ea073b441491
umbrella22 <1035765115@qq.com>
2021-07-15 16:57:30 +0800

    修正多次监听问题

--- src/main/services/downloadFile.ts
+++ src/main/services/downloadFile.ts
@@ -41,39 +41,40 @@ class Main {
           this.mainWindow.webContents.downloadURL(this.downloadUrl)
         } catch (error) { console.log(error) }
       })
-      this.mainWindow.webContents.session.on('will-download', (event: any, item: any, webContents: any) => {
-        const filePath = join(app.getPath('downloads'), item.getFilename())
-        item.setSavePath(filePath)
-        item.on('updated', (event: any, state: String) => {
-          switch (state) {
-            case 'progressing':
-              this.mainWindow.webContents.send('download-progress', (item.getReceivedBytes() / item.getTotalBytes() * 100).toFixed(0))
-              break
-            default:
-              this.mainWindow.webContents.send('download-error', true)
-              dialog.showErrorBox('下载出错', '由于网络或其他未知原因导致客户端下载出错，请前往官网进行重新安装')
-              break
-          }
-        })
-        item.once('done', (event: any, state: String) => {
-          switch (state) {
-            case 'completed':
-              const data = {
-                filePath
-              }
-              this.mainWindow.webContents.send('download-done', data)
-              break
-            case 'interrupted':
-              this.mainWindow.webContents.send('download-error', true)
-              dialog.showErrorBox('下载出错', '由于网络或其他未知原因导致客户端下载出错，请前往官网进行重新安装')
-              break
-            default:
-              break
-          }
-        })
+    })
+    this.mainWindow.webContents.session.on('will-download', (event: any, item: any, webContents: any) => {
+      const filePath = join(app.getPath('downloads'), item.getFilename())
+      item.setSavePath(filePath)
+      item.on('updated', (event: any, state: String) => {
+        switch (state) {
+          case 'progressing':
+            this.mainWindow.webContents.send('download-progress', (item.getReceivedBytes() / item.getTotalBytes() * 100).toFixed(0))
+            break
+          default:
+            this.mainWindow.webContents.send('download-error', true)
+            dialog.showErrorBox('下载出错', '由于网络或其他未知原因导致客户端下载出错，请前往官网进行重新安装')
+            break
+        }
+      })
+      item.once('done', (event: any, state: String) => {
+        switch (state) {
+          case 'completed':
+            const data = {
+              filePath
+            }
+            this.mainWindow.webContents.send('download-done', data)
+            break
+          case 'interrupted':
+            this.mainWindow.webContents.send('download-error', true)
+            dialog.showErrorBox('下载出错', '由于网络或其他未知原因导致客户端下载出错，请前往官网进行重新安装')
+            break
+          default:
+            break
+        }
       })
     })
   }
+
 }
 
 export default Main
