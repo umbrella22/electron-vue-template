fad24a6e19274ffc44e0eb77f98c339e34e2c9ef
umbrella22 <1035765115@qq.com>
2022-08-31 18:59:28 +0800

    fix: dele error compont

--- src/renderer/views/form/index.vue
+++ src/renderer/views/form/index.vue
@@ -10,20 +10,6 @@
           <el-option label="北京" value="北京"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="活动时间">
-        <el-col :span="11">
-          <el-date-picker type="date" placeholder="选择时间" v-model="form.date1" style="width: 100%;"></el-date-picker>
-        </el-col>
-        <el-col class="line" :span="2">-</el-col>
-        <el-col :span="11">
-          <el-time-picker
-            type="fixed-time"
-            placeholder="选择时间"
-            v-model="form.date2"
-            style="width: 100%;"
-          ></el-time-picker>
-        </el-col>
-      </el-form-item>
       <el-form-item label="即时交付">
         <el-switch v-model="form.delivery"></el-switch>
       </el-form-item>
