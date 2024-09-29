4f235bd136750785fb57097b8301150cc593edd6
umbrella22 <1035765115@qq.com>
2024-09-29 17:53:28 +0800

    chore: 修复热更新中，解压包重复的问题

--- src/main/services/hot-updater.ts
+++ src/main/services/hot-updater.ts
@@ -2,23 +2,23 @@
  * power by biuuu
  */
 
-import { emptyDir, createWriteStream, readFile, copy, remove } from "fs-extra";
-import { join, resolve } from "path";
-import { promisify } from "util";
-import { pipeline } from "stream";
-import { app, BrowserWindow } from "electron";
-import { gt } from "semver";
-import { createHmac } from "crypto";
-import extract from "extract-zip";
-import { version } from "../../../package.json";
-import { hotPublishConfig } from "../config/hot-publish";
-import axios, { AxiosResponse } from "axios";
-import { webContentSend } from "./web-content-send";
+import { emptyDir, createWriteStream, readFile, copy, remove } from 'fs-extra'
+import { join, resolve } from 'path'
+import { promisify } from 'util'
+import { pipeline } from 'stream'
+import { app, BrowserWindow } from 'electron'
+import { gt } from 'semver'
+import { createHmac } from 'crypto'
+import AdmZip from 'adm-zip'
+import { version } from '../../../package.json'
+import { hotPublishConfig } from '../config/hot-publish'
+import axios, { AxiosResponse } from 'axios'
+import { webContentSend } from './web-content-send'
 
-const streamPipeline = promisify(pipeline);
-const appPath = app.getAppPath();
-const updatePath = resolve(appPath, "..", "..", "update");
-const request = axios.create();
+const streamPipeline = promisify(pipeline)
+const appPath = app.getAppPath()
+const updatePath = resolve(appPath, '..', '..', 'update')
+const request = axios.create()
 
 /**
  * @param data 文件流
@@ -28,10 +28,10 @@ const request = axios.create();
  * @author umbrella22
  * @date 2021-03-05
  */
-function hash(data: Buffer, type = "sha256", key = "Sky"): string {
-  const hmac = createHmac(type, key);
-  hmac.update(data);
-  return hmac.digest("hex");
+function hash(data: Buffer, type = 'sha256', key = 'Sky'): string {
+  const hmac = createHmac(type, key)
+  hmac.update(data)
+  return hmac.digest('hex')
 }
 
 /**
@@ -42,21 +42,21 @@ function hash(data: Buffer, type = "sha256", key = "Sky"): string {
  * @date 2021-03-05
  */
 async function download(url: string, filePath: string): Promise<void> {
-  const res = await request({ url, responseType: "stream" });
-  await streamPipeline(res.data, createWriteStream(filePath));
+  const res = await request({ url, responseType: 'stream' })
+  await streamPipeline(res.data, createWriteStream(filePath))
 }
 
 const updateInfo = {
-  status: "init",
-  message: "",
-};
+  status: 'init',
+  message: '',
+}
 
 interface Res extends AxiosResponse<any> {
   data: {
-    version?: string;
-    name?: string;
-    hash?: string;
-  };
+    version: string
+    name: string
+    hash: string
+  }
 }
 
 /**
@@ -71,36 +71,36 @@ export const updater = async (windows?: BrowserWindow): Promise<void> => {
       url: `${hotPublishConfig.url}/${
         hotPublishConfig.configName
       }.json?time=${new Date().getTime()}`,
-    });
+    })
     if (gt(res.data.version, version)) {
-      await emptyDir(updatePath);
-      const filePath = join(updatePath, res.data.name);
-      updateInfo.status = "downloading";
+      await emptyDir(updatePath)
+      const filePath = join(updatePath, res.data.name)
+      updateInfo.status = 'downloading'
       if (windows)
-        webContentSend.HotUpdateStatus(windows.webContents, updateInfo);
-      await download(`${hotPublishConfig.url}/${res.data.name}`, filePath);
-      const buffer = await readFile(filePath);
-      const sha256 = hash(buffer);
-      if (sha256 !== res.data.hash) throw new Error("sha256 error");
-      const appPathTemp = join(updatePath, "temp");
-      await extract(filePath, { dir: appPathTemp });
-      updateInfo.status = "moving";
+        webContentSend.HotUpdateStatus(windows.webContents, updateInfo)
+      await download(`${hotPublishConfig.url}/${res.data.name}`, filePath)
+      const buffer = await readFile(filePath)
+      const sha256 = hash(buffer)
+      if (sha256 !== res.data.hash) throw new Error('sha256 error')
+      const appPathTemp = join(updatePath, 'temp')
+      const zip = new AdmZip(filePath)
+      zip.extractAllTo(appPathTemp, true, true)
+      updateInfo.status = 'moving'
       if (windows)
-        webContentSend.HotUpdateStatus(windows.webContents, updateInfo);
-      await remove(join(`${appPath}`, "dist"));
-      await remove(join(`${appPath}`, "package.json"));
-      await copy(appPathTemp, appPath);
-      updateInfo.status = "finished";
+        webContentSend.HotUpdateStatus(windows.webContents, updateInfo)
+      await remove(join(`${appPath}`, 'dist'))
+      await remove(join(`${appPath}`, 'package.json'))
+      await copy(appPathTemp, appPath)
+      updateInfo.status = 'finished'
       if (windows)
-        webContentSend.HotUpdateStatus(windows.webContents, updateInfo);
+        webContentSend.HotUpdateStatus(windows.webContents, updateInfo)
     }
   } catch (error) {
-    updateInfo.status = "failed";
-    updateInfo.message = error;
+    updateInfo.status = 'failed'
+    updateInfo.message = error
 
-    if (windows)
-      webContentSend.HotUpdateStatus(windows.webContents, updateInfo);
+    if (windows) webContentSend.HotUpdateStatus(windows.webContents, updateInfo)
   }
-};
+}
 
-export const getUpdateInfo = () => updateInfo;
+export const getUpdateInfo = () => updateInfo
