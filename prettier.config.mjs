792244c8a205380930254453f97731bd2f2d94d3
umbrella22 <1035765115@qq.com>
2025-06-09 10:24:11 +0800

    feat: 更新依赖，并重新生产锁文件，解决部分依赖问题

---
+++ prettier.config.mjs
@@ -0,0 +1,16 @@
+import {
+  endOfLine,
+  htmlWhitespaceSensitivity,
+  printWidth,
+  semi,
+  singleQuote,
+  trailingComma,
+} from '@ikaros-cli/prettier-config'
+export default {
+  endOfLine,
+  htmlWhitespaceSensitivity,
+  printWidth,
+  semi,
+  singleQuote,
+  trailingComma,
+}
